076ae4cfb5d3299e5e03e99cf7873ad72e6199bc
Blade Barringer <blade@crookedneighbor.com>
2015-12-01 17:43:04 -0600

    feat(lib): Write library for manipulating dom

---
+++ src/js/dom.js
@@ -0,0 +1,71 @@
+export function createKeyListener (options = {}) {
+  let { habitica, task } = options;
+  let keys = options.keys || [];
+
+  let altKey = 18;
+  let shiftKey = 16;
+  let sKey = 83;
+
+  let keyListener = (e) => {
+    let isPressed = e.type === 'keydown';
+
+    keys[e.keyCode] = isPressed;
+
+    if (keys[altKey] && keys[shiftKey] && keys[sKey]) {
+      habitica.task.post(task).then(() => {
+        displayMessage(null, 'To-Do Created!');
+      }).catch(displayMessage);
+    }
+  };
+
+  return keyListener;
+}
+
+export function addFunctionToKeyEvents (fn, dom = document) {
+  dom.addEventListener('keydown', fn);
+  dom.addEventListener('keyup', fn);
+}
+
+export function displayMessage (err, successMsg, dom = document) {
+  let message = dom.createElement('div');
+
+  message.classList.add('send-to-habitica-msg');
+
+  if (err) {
+    message.classList.add('send-to-habitica-msg-error');
+    message.innerText = `ERROR: ${err.text}`;
+  } else {
+    message.classList.add('send-to-habitica-msg-success');
+    message.innerText = successMsg;
+  }
+  dom.body.appendChild(message);
+
+  let tasks = [{
+    fn: _makeElementAppear,
+    time: 100,
+  }, {
+    fn: _makeElementDissapear,
+    time: 4000,
+  }, {
+    fn: _removeElement,
+    time: 5000,
+  }];
+
+  tasks.map((task) => {
+    setTimeout(() => {
+      task.fn(message);
+    }, task.time);
+  });
+}
+
+function _makeElementAppear (element) {
+  element.style.top = '20px';
+}
+
+function _makeElementDissapear (element) {
+  element.style.opacity = 0;
+}
+
+function _removeElement (element) {
+  element.remove();
+}
---
+++ test/dom.test.js
@@ -0,0 +1,274 @@
+import {
+  createKeyListener,
+  addFunctionToKeyEvents,
+  displayMessage,
+} from '../src/js/dom';
+
+describe('dom', () => {
+  describe('#createKeyListener', () => {
+    let options, habitica, keys, cb;
+
+    beforeEach(() => {
+      habitica = {
+        task: {
+          post: sinon.stub().returns({
+            then: sinon.stub().returnsThis(),
+            catch: sinon.stub().returnsThis(),
+          }),
+        },
+      };
+      keys = [];
+      cb = sinon.stub();
+
+      options = {
+        habitica,
+        keys,
+        cb,
+      };
+    });
+
+    context('setup', () => {
+      it('returns a function', () => {
+        let keyListener = createKeyListener(options);
+
+        expect(keyListener).to.be.a('function');
+      });
+    });
+
+    context('key listener', () => {
+      it('keeps track of pressed down keys', () => {
+        let keyListener = createKeyListener(options);
+        let evt1 = {
+          type: 'keydown',
+          keyCode: 50,
+        };
+        let evt2 = {
+          type: 'keydown',
+          keyCode: 52,
+        };
+        let evt3 = {
+          type: 'something-else',
+          keyCode: 54,
+        };
+
+        keyListener(evt1);
+        keyListener(evt2);
+        keyListener(evt3);
+
+        expect(keys[50]).to.eql(true);
+        expect(keys[52]).to.eql(true);
+        expect(keys[54]).to.eql(false);
+      });
+
+      it('toggles actively pressed keys', () => {
+        let keyListener = createKeyListener(options);
+        let evt1 = {
+          type: 'keydown',
+          keyCode: 50,
+        };
+        let evt2 = {
+          type: 'keyup',
+          keyCode: 50,
+        };
+
+        keyListener(evt1);
+        expect(keys[50]).to.eql(true);
+
+        keyListener(evt2);
+        expect(keys[50]).to.eql(false);
+      });
+
+      it('sends habitica request when alt + shift + s key are pressed', () => {
+        options.task = {
+          text: 'page title',
+          type: 'todo',
+          notes: 'page url',
+        };
+        let keyListener = createKeyListener(options);
+        let altKey = 18;
+        let shiftKey = 16;
+        let sKey = 83;
+        let evt = {
+          keyCode: sKey,
+          type: 'keydown',
+        };
+
+        keys[altKey] = true; // alt
+        keys[shiftKey] = true; // shift
+
+        keyListener(evt);
+
+        expect(habitica.task.post).to.be.calledOnce;
+        expect(habitica.task.post).to.be.calledWith({
+          text: 'page title',
+          type: 'todo',
+          notes: 'page url',
+        });
+      });
+
+      it('does not send habitica request when missing shortcut key', () => {
+        let keyListener = createKeyListener(options);
+        let altKey = 18;
+        let sKey = 83;
+        let evt = {
+          keyCode: sKey,
+          type: 'keydown',
+        };
+
+        keys[altKey] = true; // alt
+
+        keyListener(evt);
+
+        expect(habitica.task.post).to.not.be.called;
+      });
+    });
+  });
+
+  describe('#addFunctionToKeyEvents', () => {
+    let dom, fn;
+
+    beforeEach(() => {
+      fn = sinon.stub();
+      dom = {
+        addEventListener: sinon.stub(),
+      };
+    });
+
+    it('adds keyListener to keydown event', () => {
+      addFunctionToKeyEvents(fn, dom);
+
+      expect(dom.addEventListener).to.be.calledWith('keydown', fn);
+    });
+
+    it('adds keyListener to keyup event', () => {
+      addFunctionToKeyEvents(fn, dom);
+
+      expect(dom.addEventListener).to.be.calledWith('keyup', fn);
+    });
+  });
+
+  describe('#displayMessage', () => {
+    let dom, element, clock;
+
+    beforeEach(() => {
+      clock = sinon.useFakeTimers();
+      element = {
+        classList: { add: sinon.stub() },
+        style: {},
+        remove: sinon.stub(),
+      };
+      dom = {
+        createElement: sinon.stub().returns(element),
+        body: {
+          appendChild: sinon.stub(),
+        },
+      };
+    });
+
+    afterEach(() => {
+      clock.restore();
+    });
+
+    context('errors', () => {
+      it('adds error message to element\'s text', () => {
+        displayMessage({text: 'some error'}, null, dom);
+
+        expect(element.innerText).to.eql('ERROR: some error');
+      });
+
+      it('applies msg classes', () => {
+        displayMessage({text: 'some error'}, null, dom);
+
+        expect(element.classList.add).to.be.calledWith('send-to-habitica-msg');
+        expect(element.classList.add).to.be.calledWith('send-to-habitica-msg-error');
+      });
+
+      it('adds element to body', () => {
+        displayMessage({text: 'some error'}, null, dom);
+
+        expect(dom.body.appendChild).to.be.calledWith(element);
+      });
+
+      it('adjusts style of element to appear after 100 miliseconds', () => {
+        displayMessage({text: 'some error'}, null, dom);
+
+        clock.tick(99);
+        expect(element.style).to.not.have.property('top');
+
+        clock.tick(1);
+        expect(element.style).to.have.property('top', '20px');
+      });
+
+      it('makes element dissapear after 4000 miliseconds', () => {
+        displayMessage({text: 'some error'}, null, dom);
+
+        clock.tick(3999);
+        expect(element.style).to.not.have.property('opacity');
+
+        clock.tick(1);
+        expect(element.style).to.have.property('opacity', 0);
+      });
+
+      it('removes element from dom after 5000 miliseconds', () => {
+        displayMessage({text: 'some error'}, null, dom);
+
+        clock.tick(4999);
+        expect(element.remove).to.not.be.called;
+
+        clock.tick(1);
+        expect(element.remove).to.be.calledOnce;
+      });
+    });
+
+    context('success', () => {
+      it('adds success message to element\'s text', () => {
+        displayMessage(null, 'success!', dom);
+
+        expect(element.innerText).to.eql('success!');
+      });
+
+      it('applies msg classes', () => {
+        displayMessage(null, 'success!', dom);
+
+        expect(element.classList.add).to.be.calledWith('send-to-habitica-msg');
+        expect(element.classList.add).to.be.calledWith('send-to-habitica-msg-success');
+      });
+
+      it('adds element to body', () => {
+        displayMessage(null, 'success!', dom);
+
+        expect(dom.body.appendChild).to.be.calledWith(element);
+      });
+
+      it('adjusts style of element to appear after 100 miliseconds', () => {
+        displayMessage(null, 'success!', dom);
+
+        clock.tick(99);
+        expect(element.style).to.not.have.property('top');
+
+        clock.tick(1);
+        expect(element.style).to.have.property('top', '20px');
+      });
+
+      it('makes element dissapear after 4000 miliseconds', () => {
+        displayMessage(null, 'success!', dom);
+
+        clock.tick(3999);
+        expect(element.style).to.not.have.property('opacity');
+
+        clock.tick(1);
+        expect(element.style).to.have.property('opacity', 0);
+      });
+
+      it('removes element from dom after 5000 miliseconds', () => {
+        displayMessage(null, 'success!', dom);
+
+        clock.tick(4999);
+        expect(element.remove).to.not.be.called;
+
+        clock.tick(1);
+        expect(element.remove).to.be.calledOnce;
+      });
+    });
+  });
+});
